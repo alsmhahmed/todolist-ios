5394ce31236b9b5643bef1f182b843ee0ed56cd6
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-11-23 14:23:42 +0200

    Initial Commit

---
+++ todolistnew/todolistnewApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistnewApp.swift
+//  todolistnew
+//
+//  Created by alsmh ahmed on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct todolistnewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
